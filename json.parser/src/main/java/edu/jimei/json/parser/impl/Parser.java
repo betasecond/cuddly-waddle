a9e48afcb630ac9b280dc80d0be3f15be35867fa
betasecond <nbdhc9039@gmail.coom>
2024-05-01 10:04:08 +0800

    finish parser.json

--- json.parser/src/main/java/edu/jimei/json/parser/impl/Parser.java
+++ json.parser/src/main/java/edu/jimei/json/parser/impl/Parser.java
@@ -1,20 +1,24 @@
 package edu.jimei.json.parser.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.jimei.ics.struct.CourseSchedule;
 import edu.jimei.json.parser.ParserService;
+import edu.jimei.utils.JacksonConfig;
 
 public class Parser implements ParserService {
+
     /**
-     * @param json
-     * @return
+     * Parses the given JSON string into a CourseSchedule object.
+     * @param json JSON string representing a CourseSchedule.
+     * @return Parsed CourseSchedule object or null if parsing fails.
      */
     @Override
     public CourseSchedule parse(String json) {
-        return null;
+        try {
+            return JacksonConfig.deserialize(json, CourseSchedule.class);
+        } catch (JsonProcessingException e) {
+            System.err.println("Failed to parse JSON: " + e.getMessage());
+            return null;
+        }
     }
-
-    private CourseSchedule courseSchedule(){
-
-        return null;
-    };
 }
