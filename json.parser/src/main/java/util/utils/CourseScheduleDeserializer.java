6f659375375730ebd655547a206a96c4bc3031ff
betasecond <nbdhc9039@gmail.coom>
2024-05-01 15:19:09 +0800

    code style fix

--- json.parser/src/main/java/util/utils/CourseScheduleDeserializer.java
+++ json.parser/src/main/java/util/utils/CourseScheduleDeserializer.java
@@ -1,6 +1,6 @@
 package util.utils;
 
-import com.fasterxml.jackson.core.JacksonException;
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
